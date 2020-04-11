a1149df5c0d74cebc15250333c6488e0b44bffec
Sumanth V Madyalkar <sumanth.v.m@in.ibm.com>
2020-04-11 14:26:55 +0530

    Corrected code

--- DSL/MavenProjectDSL.groovy
+++ DSL/MavenProjectDSL.groovy
@@ -1,11 +1,10 @@
 job('First-Maven-Project-DSL'){
     description("This is the first job created using the DSL jenkins file on ${new Date()}")
-
-    triggers{
-        scm('* * * * *')
-    }
     scm{
-        github("https://github.com/sumanthvm/Jenkins_Upgradev3.git", master)
+        github("https://github.com/sumanthvm/Jenkins_Upgradev3.git", 'master')
+    }
+    triggers{
+       scm('* * * * *')
     }
     steps{
         maven('clean package', 'maven-samples/single-module/pom.xml')
