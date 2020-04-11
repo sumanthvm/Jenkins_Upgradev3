d178992c32d13cdba7a8b01bcf87e0e780e7e5de
Sumanth V Madyalkar <sumanth.v.m@in.ibm.com>
2020-04-11 14:15:29 +0530

    New changes

--- DSL/MavenProjectDSL.groovy
+++ DSL/MavenProjectDSL.groovy
@@ -1,6 +1,6 @@
 job('First-Maven-Project-DSL'){
     description("This is the first job created using the DSL jenkins file on ${new Date()}")
-//    logRotator(int daysToKeep = -1, int numToKeep = -1, int artifactDaysToKeep = -1, int artifactNumToKeep = -1)
+
     triggers{
         scm('@mintue')
     }
